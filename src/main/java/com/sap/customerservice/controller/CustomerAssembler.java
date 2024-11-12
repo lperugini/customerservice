785aaa783e4ea6ebc5a9102df09386507decf807
Leonardo Perugini <lperugini2@gmail.com>
2024-11-12 20:55:27 +0100

    Updated pom configuration file

--- src/main/java/com/sap/customerservice/controller/CustomerAssembler.java
+++ src/main/java/com/sap/customerservice/controller/CustomerAssembler.java
@@ -3,6 +3,7 @@ package com.sap.customerservice.controller;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 import org.springframework.hateoas.EntityModel;
+import org.springframework.lang.NonNull;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +12,7 @@ import com.sap.customerservice.model.Customer;
 @Component
 public class CustomerAssembler implements RepresentationModelAssembler<Customer, EntityModel<Customer>> {
     @Override
-    public EntityModel<Customer> toModel(Customer customer) {
+    public @NonNull EntityModel<Customer> toModel(@NonNull Customer customer) {
 
         return EntityModel.of(customer, //
                 linkTo(methodOn(CustomerController.class).one(customer.getId())).withSelfRel(),
